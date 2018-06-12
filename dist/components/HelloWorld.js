59f9b96384907bac71af3ac5bc79cfc77d4ebb20
foreveross_xiaoxinlin <xiaoxinlin@foreveross.com>
2018-06-12 15:25:25 +0800

    init

--- dist/components/HelloWorld.js
+++ dist/components/HelloWorld.js
@@ -96,7 +96,7 @@ __vue_options__ = __vue_exports__ = __vue_exports__.default
 if (typeof __vue_options__ === "function") {
   __vue_options__ = __vue_options__.options
 }
-__vue_options__.__file = "/Users/xiaoxinlin/Documents/work/text2/t2/src/components/HelloWorld.vue"
+__vue_options__.__file = "/Users/xiaoxinlin/Documents/work/t2/src/components/HelloWorld.vue"
 __vue_options__.render = __vue_template__.render
 __vue_options__.staticRenderFns = __vue_template__.staticRenderFns
 __vue_options__.style = __vue_options__.style || {}
--- dist/index.js
+++ dist/index.js
@@ -104,10 +104,10 @@ __vue_options__ = __vue_exports__ = __vue_exports__.default
 if (typeof __vue_options__ === "function") {
   __vue_options__ = __vue_options__.options
 }
-__vue_options__.__file = "/Users/xiaoxinlin/Documents/work/text2/t2/src/index.vue"
+__vue_options__.__file = "/Users/xiaoxinlin/Documents/work/t2/src/index.vue"
 __vue_options__.render = __vue_template__.render
 __vue_options__.staticRenderFns = __vue_template__.staticRenderFns
-__vue_options__._scopeId = "data-v-014dcfc3"
+__vue_options__._scopeId = "data-v-b095e84e"
 __vue_options__.style = __vue_options__.style || {}
 __vue_styles__.forEach(function (module) {
   for (var name in module) {
@@ -208,7 +208,7 @@ __vue_options__ = __vue_exports__ = __vue_exports__.default
 if (typeof __vue_options__ === "function") {
   __vue_options__ = __vue_options__.options
 }
-__vue_options__.__file = "/Users/xiaoxinlin/Documents/work/text2/t2/src/components/HelloWorld.vue"
+__vue_options__.__file = "/Users/xiaoxinlin/Documents/work/t2/src/components/HelloWorld.vue"
 __vue_options__.render = __vue_template__.render
 __vue_options__.staticRenderFns = __vue_template__.staticRenderFns
 __vue_options__.style = __vue_options__.style || {}
